54244346082b616322278e4167e1d0694bde3849
kaiser-3007 <sddrthpkhrna@gmail.com>
2020-10-26 01:12:29 +0530

    [add] orbit controls

--- src/pages/Form/index.js
+++ src/pages/Form/index.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import { Canvas } from 'react-three-fiber';
+import React, { useRef } from 'react';
+import { Canvas, useFrame, useThree, extend } from 'react-three-fiber';
 import GroundPlane from '../../components/GroundPlane';
 import BackDrop from '../../components/BackDrop';
 import { PointLight } from '../../components/Lights';
 import FormComponent from '../../components/Form';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+extend({ OrbitControls });
+
+const CameraControls = () => {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+  const controls = useRef();
+
+  useFrame((state) => controls.current.update());
+
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      enableZoom={true}
+      enablePan={false}
+      enableRotate={false}
+      // maxAzimuthAngle={Math.PI / 4}
+      // maxPolarAngle={Math.PI}
+      // minAzimuthAngle={-Math.PI / 4}
+      // minPolarAngle={0}
+    />
+  );
+};
 
 const Form = () => {
   return (
     <Canvas camera={{ position: [0, 0.25, 13] }}>
+      <CameraControls />
       <GroundPlane 
         color="#7F7D7E" />
 
